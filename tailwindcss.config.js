822997e04e76a48c4f1d0233eea863ff487ea1a6
Anji-04 <anjoli_mehta@yahoo.com>
2025-11-23 21:24:56 -0500

    Update tailwindcss.config.js

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,12 +1,16 @@
 export default {
   content: [
     "./index.html",
-    "./src/*/.{js,ts,jsx,tsx}",
-    "./components/*/.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./App.tsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        border: '#your-color-value', // add your desired border color here
+      },
+    },
   },
   plugins: [],
 }
